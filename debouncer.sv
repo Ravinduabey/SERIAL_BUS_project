96a933cccf46f8382c5ccf628fb1b732f84e5bfa
unknown <tharindu.samare123@gmail.com>
2021-08-03 13:55:39 +0530

    initial commit

---
+++ debouncer.sv
@@ -0,0 +1,34 @@
+module debouncer 
+(
+    input logic clk,
+    input logic value_in,
+    output logic value_out
+);
+
+localparam TIME_DELAY = 500; // mS
+localparam CLK_FREQ = 50; // MHz
+localparam MAX_CLK_COUNT = TIME_DELAY * CLK_FREQ * 1000;
+localparam COUNTER_WIDTH = $clog2(MAX_CLK_COUNT);
+
+logic [COUNTER_WIDTH-1:0]counter;
+
+always_ff @(posedge clk) begin
+    if (value_in == 1'b0) begin
+        counter <= counter + COUNTER_WIDTH'(1);
+    end
+    else if (counter == MAX_CLK_COUNT) begin
+        counter <= '0;
+    end
+    else if (counter != '0) begin
+        counter <= counter + COUNTER_WIDTH'(1);
+    end
+end
+
+assign value_out = (counter == '0)? value_in: 1'b0;  // while counting value_out is always zero
+
+
+
+
+
+
+endmodule : debouncer
---
+++ master.sv
@@ -0,0 +1,9 @@
+module master 
+(
+    input logic clk, rstN,
+    output logic control,write_data,valid,last,
+    input logic read_data, ready
+);
+
+
+endmodule: master
