96ee007407ad9599f122047f06418a9ad9f9d866
MuhammedImdaad <muhammedimdaad16@gmail.com>
2021-08-05 09:36:26 +0530

    added arbiter pinouts

---
+++ src/arbiter.sv
@@ -0,0 +1,14 @@
+module arbiter(
+    input logic clk,
+	 input logic rstn,
+    input logic m1_in,
+	 input logic m2_in,
+	 output logic m1_out,
+	 output logic m2_out,
+	 input logic ready,
+	 output logic buscontrol
+);
+
+
+
+endmodule : arbiter
