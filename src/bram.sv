9f5e8c1c76bb5574e09a62624bddb49a11e4ea60
Ravinduabey <ravinduabey@gmail.com>
2021-08-07 16:16:49 +0530

    updated parameter names

--- src/bram.sv
+++ src/bram.sv
@@ -1,19 +1,19 @@
 module bram #(
-    parameter ADDRESS_DEPTH = 4092,
+    parameter MEMORY_DEPTH = 4092,
     parameter DATA_WIDTH = 16
 )(
     input   logic                                       clk,
     input   logic                                       wr,
     input   logic   [DATA_WIDTH-1:0]                    data,
-    input   logic   [$clog2(ADDRESS_DEPTH)-1:0]         address,
+    input   logic   [ADDRESS_WIDTH-1:0]         address,
     
     output  logic   [DATA_WIDTH-1:0]                    q
 );
 
-    logic [DATA_WIDTH-1:0] ram[ADDRESS_DEPTH-1:0];
+    logic [DATA_WIDTH-1:0] ram[MEMORY_DEPTH-1:0];
+	localparam ADDRESS_WIDTH = $clog2(MEMORY_DEPTH);
 	
-	logic [$clog2(ADDRESS_DEPTH)-1:0] addr_reg;
-	
+	logic [ADDRESS_WIDTH-1:0] addr_reg;
 	always @ (posedge clk)
 	begin
 		if (wr)                     // Write
