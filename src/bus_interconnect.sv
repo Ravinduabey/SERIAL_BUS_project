d645fdbae3e031ddbb45211cc43c90819234ebf2
NushaNawzar <nushanawzar275@gmail.com>
2021-08-16 11:04:24 +0530

    parameterized ports

--- src/bus_interconnect.sv
+++ src/bus_interconnect.sv
@@ -1,33 +1,38 @@
-module bus_interconnect (
-    //arbiter
-    input  [2:0] master, 
-    input  [2:0] slave,
+module bus_interconnect #(
+    parameter NO_MASTERS = 2,
+    parameter NO_SLAVES = 3,
+    parameter THRESH = 1000,
+    parameter S_ID_WIDTH = $clog2(NO_SLAVES+1), //2
+    parameter M_ID_WIDTH = $clog2(NO_MASTERS) //1
+)(
 
-    //master1
-    input       m1_valid, m1_last, m1_wD,
-    output      m1_ready, m1_rD,
-    //master2
-    input       m2_valid, m2_last, m2_wD,
-    output      m2_ready, m2_rD,
+    // arbiter controllers
+    input logic [M_ID_WIDTH-1:0] addr_select,
+	input logic [M_ID_WIDTH-1:0] MOSI_data_select,
+	input logic [M_ID_WIDTH-1:0] valid_select,
+	input logic [M_ID_WIDTH-1:0] last_select,
+    input logic [S_ID_WIDTH-1:0] MISO_data_select,
+	input logic [S_ID_WIDTH-1:0] ready_select,
 
-    //slave1
-    output      s1_valid, s1_last, s1_wD,
-    input       s1_ready, s1_rD,
-    //slave2
-    output      s2_valid, s2_last, s2_wD,
-    input       s2_ready, s2_rD,
-    //slave3
-    output      s3_valid, s3_last, s3_wD,
-    input       s3_ready, s3_rD
-);
+    output logic ready,
 
-mux_2to3 valid (.master(master), .slave(slave), .master1(m1_valid), .master2(m2_valid), .slave1(s1_valid), .slave2(s2_valid), .slave3(s3_valid));
-mux_2to3 last  (.master(master), .slave(slave), .master1(m1_last) , .master2(m2_last) , .slave1(s1_last) , .slave2(s2_last) , .slave3(s3_last));
-mux_2to3 wD    (.master(master), .slave(slave), .master1(m1_wD)   , .master2(m2_wD)   , .slave1(s1_wD)   , .slave2(s2_wD)   , .slave3(s3_wD));
-mux_2to3 control(.master(master), .slave(slave), .master1(control)   , .master2(control)   , .slave1(control)   , .slave2(control)   , .slave3(control));
+    //masters
+    input   logic [M_ID_WIDTH-1:0] control,
+	input   logic [M_ID_WIDTH-1:0] wD,
+	input   logic [M_ID_WIDTH-1:0] valid,
+	input   logic [M_ID_WIDTH-1:0] last,
+    output  logic [S_ID_WIDTH-1:0] rD,
+	output  logic [S_ID_WIDTH-1:0] ready,
 
-mux_3to2 ready (.master(master), .slave(slave), .master1(m1_ready), .master2(m2_ready), .slave1(s1_ready), .slave2(s2_ready), .slave3(s3_ready));
-mux_3to2 rD    (.master(master), .slave(slave), .master1(m1_rD)   , .master2(m2_rD)   , .slave1(s1_rD)   , .slave2(s2_rD)   , .slave3(s3_rD));
+    //slaves
+    output  logic [M_ID_WIDTH-1:0] control,
+	output  logic [M_ID_WIDTH-1:0] wD,
+	output  logic [M_ID_WIDTH-1:0] valid,
+	output  logic [M_ID_WIDTH-1:0] last,
+    input   logic [S_ID_WIDTH-1:0] rD,
+	input   logic [S_ID_WIDTH-1:0] ready,
+    );
+
 
 
 endmodule
--- src/bus_interconnect_tb.sv
+++ src/bus_interconnect_tb.sv
@@ -1,18 +1,37 @@
-module bus_interconnect_tb ();
-    logic [2:0] master;
-    logic [2:0] slave;
+module bus_interconnect_tb #(
+    parameter NO_MASTERS = 2,
+    parameter NO_SLAVES = 3,
+    parameter THRESH = 1000,
+    parameter S_ID_WIDTH = $clog2(NO_SLAVES+1), //2
+    parameter M_ID_WIDTH = $clog2(NO_MASTERS) //1
+)(
 
-    logic   m1_valid, m1_last, m1_wD;
-    logic   m1_ready, m1_rD;
-    logic   m2_valid, m2_last, m2_wD;
-    logic   m2_ready, m2_rD;
+    // arbiter controllers
+    input logic [M_ID_WIDTH-1:0] addr_select,
+	input logic [M_ID_WIDTH-1:0] MOSI_data_select,
+	input logic [M_ID_WIDTH-1:0] valid_select,
+	input logic [M_ID_WIDTH-1:0] last_select,
+    input logic [S_ID_WIDTH-1:0] MISO_data_select,
+	input logic [S_ID_WIDTH-1:0] ready_select,
 
-    logic   s1_valid, s1_last, s1_wD;
-    logic   s1_ready, s1_rD;
-    logic   s2_valid, s2_last, s2_wD;
-    logic   s2_ready, s2_rD;
-    logic   s3_valid, s3_last, s3_wD;
-    logic   s3_ready, s3_rD;
+    output logic ready,
+
+    //masters
+    input   logic [M_ID_WIDTH-1:0] control,
+	input   logic [M_ID_WIDTH-1:0] wD,
+	input   logic [M_ID_WIDTH-1:0] valid,
+	input   logic [M_ID_WIDTH-1:0] last,
+    output  logic [S_ID_WIDTH-1:0] rD,
+	output  logic [S_ID_WIDTH-1:0] ready,
+
+    //slaves
+    output  logic [M_ID_WIDTH-1:0] control,
+	output  logic [M_ID_WIDTH-1:0] wD,
+	output  logic [M_ID_WIDTH-1:0] valid,
+	output  logic [M_ID_WIDTH-1:0] last,
+    input   logic [S_ID_WIDTH-1:0] rD,
+	input   logic [S_ID_WIDTH-1:0] ready,
+    );
 
     logic clk;
     localparam CLOCK_PERIOD = 20;
@@ -23,21 +42,7 @@ module bus_interconnect_tb ();
             end
     end
 
-    bus_interconnect dut (
-        .master(master), .slave(slave), 
-        .m1_valid(m1_valid), .m1_last(m1_last), .m1_wD(m1_wD),
-        .m1_ready(m1_ready), .m1_rD(m1_rD),
-        .m2_valid(m2_valid), .m2_last(m2_last), .m2_wD(m2_wD),
-        .m2_ready(m2_ready), .m2_rD(m2_rD),
-
-        .s1_valid(s1_valid), .s1_last(s1_last), .s1_wD(s1_wD),
-        .s1_ready(s1_ready), .s1_rD(s1_rD),
-        .s2_valid(s2_valid), .s2_last(s2_last), .s2_wD(s2_wD),
-        .s2_ready(s2_ready), .s2_rD(s2_rD),
-        .s3_valid(s3_valid), .s3_last(s3_last), .s3_wD(s3_wD),
-        .s3_ready(s3_ready), .s3_rD(s3_rD)
-    );
-
+    
     initial begin
         @(posedge clk);
         master <= 2'b01;
